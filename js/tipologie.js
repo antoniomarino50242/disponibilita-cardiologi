ac56bf6b0488548cadb1d65a9dfd95213fe8cfd2
Antonio Marino <39876596+antoniomarino50242@users.noreply.github.com>
2025-06-19 10:17:58 +0200

    Create tipologie.js

---
+++ js/tipologie.js
@@ -0,0 +1,42 @@
+export const mappaTipologie = {
+  completo: {
+    testo: "Inserire le disponibilità per il turno COMPLETO (ECG, HC e HP)",
+    checkbox: ["ECG", "HC", "HP"]
+  },
+  "solo ecg": {
+    testo: "Inserire le disponibilità per il turno SOLO ECG",
+    checkbox: ["ECG"]
+  },
+  "ECG 100": {
+    testo: "Inserire le disponibilità per il turno ECG 100",
+    checkbox: ["ECG 100"]
+  },
+  "ECG 75": {
+    testo: "Inserire le disponibilità per il turno ECG 75",
+    checkbox: ["ECG 75"]
+  },
+  "turno HC": {
+    testo: "Inserire le disponibilità per il turno HC",
+    checkbox: ["HC"]
+  },
+  "turno HOLTER": {
+    testo: "Inserire le disponibilità per il turno HOLTER",
+    checkbox: ["HOLTER"]
+  },
+  "HC CONSUNTIVO": {
+    testo: "Inserire le disponibilità per il turno HC CONSUNTIVO",
+    checkbox: ["HC CONSUNTIVO"]
+  },
+  "HP CONSUNTIVO": {
+    testo: "Inserire le disponibilità per il turno HP CONSUNTIVO",
+    checkbox: ["HP CONSUNTIVO"]
+  },
+  "SPIROMETRIA CONSUNTIVO": {
+    testo: "Inserire le disponibilità per il turno SPIROMETRIA CONSUNTIVO",
+    checkbox: ["SPIROMETRIA CONSUNTIVO"]
+  },
+  "POLISONNOGRAFIA CONSUNTIVO": {
+    testo: "Inserire le disponibilità per il turno POLISONNOGRAFIA CONSUNTIVO",
+    checkbox: ["POLISONNOGRAFIA CONSUNTIVO"]
+  }
+};
